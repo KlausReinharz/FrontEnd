fb1b3235aa7a6b5cfcaae83e9509beaa9c67c7c0
Alex Ponce <alexponce2753@gmail.com>
2023-12-04 21:08:59 -0500

    nuevos cambios pero no el final

---
+++ src/app/persona.ts
@@ -0,0 +1,8 @@
+export class Persona{
+    idPersona: number;
+    nombre: String;
+    apellido: String;
+    identificacion: String;
+    fechanacimiento: Date;
+
+}
---
+++ src/app/usuario.ts
@@ -0,0 +1,8 @@
+export class Usuario{
+    idUsuario: number;
+    username: string;
+    password: string ;
+    email: string;
+    sessionactive: boolean;
+    status: string; 
+}
